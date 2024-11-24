5a8a21a9d34f6c69067be3d1f590ff9c279f4c69
jsimo <jsimonbumos@gmail.com>
2024-11-24 14:39:13 +0100

    Agregado .vs/ al .gitignore

---
+++ src/pages/post1.js
@@ -0,0 +1,54 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const UsingSSR = ({ serverData }) => {
+  return (
+    <Layout>
+      <h1>
+        <b>Post 1 del blog</b>
+      </h1>
+      <p>
+        Esta es la entrada numero 1 del blog" "}
+        <code>dog.ceo/api/breed/shiba/images/random</code>:
+      </p>
+      <img
+        style={{ width: "320px", borderRadius: "var(--border-radius)" }}
+        alt="A random dog"
+        src={serverData.message}
+      />
+      <p>
+        To learn more, head over to our{" "}
+        <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/server-side-rendering/">
+          documentation about Server Side Rendering
+        </a>
+        .
+      </p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="Post 1 del blog" />
+
+export default UsingSSR
+
+export async function getServerData() {
+  try {
+    const res = await fetch(`https://dog.ceo/api/breed/shiba/images/random`)
+    if (!res.ok) {
+      throw new Error(`Response failed`)
+    }
+    return {
+      props: await res.json(),
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      headers: {},
+      props: {},
+    }
+  }
+}
